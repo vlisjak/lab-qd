ee9c98dbe8a5ab858af98704ebc1ba0bd5e387f5
Valentin Lisjak <vlisjak@gmail.com>
2025-03-27 15:39:49 +0000

    finalised lab_fetch_configs.py: napalm getters instead of netmiko_send

--- scripts/lab_fetch_configs.py
+++ scripts/lab_fetch_configs.py
@@ -6,41 +6,28 @@ from nornir_utils.plugins.functions import print_result
 from nornir_netmiko.tasks import netmiko_send_command
 from nornir_utils.plugins.tasks.files import write_file
 from nornir_napalm.plugins.tasks import napalm_get
+from nornir.core.filter import F
 import os
 import sys
 
-# TODO: this is a very basic script that works only for xrd nodes!
-
-show_run_cmd = {"iosxr": "show run formal", "ios": "show run"}
-
-
 def backup_config(task):
-    output = task.run(task=netmiko_send_command, command_string=show_run_cmd[task.host.platform])
+    output = task.run(task=napalm_get, getters=['config'])
     task.run(
         task=write_file,
         filename=f"device_configs/{task.host}-backup.cfg",
-        content=output[0].result,
+        content=output[0].result['config']['running'],
     )
-
 
 if __name__ == "__main__":
 
-    # os.chdir(sys.path[0])
     os.makedirs("device_configs", exist_ok=True)
 
-    nr = InitNornir(config_file="nornir/nornir_config.yaml", dry_run=False)
+    nr = InitNornir(config_file="nornir/nornir_config.yaml")
 
-    # results = nr.run(task=backup_config)
-    # print_result(results)
+    # Exclude linux host
+    nr = nr.filter(~F(platform__eq="linux"))
 
-    # Easier to use napalm_get:
-    results = nr.run(task=napalm_get, getters=['config'])
-    for hostname in results:
-        print(hostname)
-        try:
-            print(results[hostname][0].result["config"]["running"])
-            print(50*'-')
-        except:
-            print(f"% Failed to fetch running config for device: {hostname}.")
-            print(50*'-')
+    results = nr.run(task=backup_config)
+    print_result(results, vars=["stdout"])
+
 
